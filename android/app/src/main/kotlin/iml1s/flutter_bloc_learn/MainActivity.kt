7258df0788d17ae556024509b8558144d457d691
ImL1s <aa22396584@gmail.com>
2019-10-07 18:52:43 +0800

    first commit

---
+++ android/app/src/main/kotlin/iml1s/flutter_bloc_learn/MainActivity.kt
@@ -0,0 +1,13 @@
+package iml1s.flutter_bloc_learn
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/database/database.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseProvider {
+  static final DatabaseProvider databaseProvider = DatabaseProvider();
+  Database _database;
+
+  final todoTable = 'Todo';
+
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await createDatabase();
+    return _database;
+  }
+
+  createDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, "ReactiveTodo.db");
+
+    var databasee = await openDatabase(path,
+        version: 1, onCreate: initDb, onUpgrade: onUpgrade);
+
+    return databasee;
+  }
+
+  FutureOr<void> initDb(Database db, int version) async {
+    await db.execute("CREATE TABLE $todoTable ("
+        "id INTEGER PRIMARY KEY, "
+        "description TEXT, "
+        "is_done INTEGER "
+        ")");
+  }
+
+  FutureOr<void> onUpgrade(Database db, int oldVersion, int newVersion) {
+    if(newVersion > oldVersion){
+      // todo
+    }
+  }
+}
